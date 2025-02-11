5b5fb98826506a3728024f371dcd3b21e5521cef
Taylor <taylorray.tray@gmail.com>
2025-02-11 07:56:45 +0200

    Add auth project

---
+++ projects/auth/src/lib/auth.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-auth',
+  imports: [],
+  template: `
+    <p>
+      auth works!
+    </p>
+  `,
+  styles: ``
+})
+export class AuthComponent {
+
+}
