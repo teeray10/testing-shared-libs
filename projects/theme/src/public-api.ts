a452b5c550896526d22bc2ff0453b1dba5b455d6
Taylor <taylorray.tray@gmail.com>
2025-02-11 07:54:59 +0200

    Initial theme POC

--- projects/theme/src/public-api.ts
+++ projects/theme/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of theme
  */
 
-export * from './lib/theme.service';
-export * from './lib/theme.component';
+// export * from './lib/theme.service';
+// export * from './lib/theme.component';
